811616af4ae52ec57dd11063635389b0e7c126a4
Anuchit Chalothorn <anoochit@gmail.com>
2014-07-17 21:34:48 +0700

    add share intent and share menu

--- mobile/src/main/java/net/redlinesoft/hellowear/MainActivity.java
+++ mobile/src/main/java/net/redlinesoft/hellowear/MainActivity.java
@@ -1,9 +1,13 @@
 package net.redlinesoft.hellowear;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
@@ -27,10 +31,34 @@ public class MainActivity extends Activity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
+       int id = item.getItemId();
+        if (id == R.id.action_share) {
+            // share to another app
+            shareToText();            
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void shareToText() {
+
+        String shareString = getString(R.string.share_string);
+        PackageInfo packageInfo;
+        try {
+            // get package info
+            packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            shareString = packageInfo.packageName;
+            
+            // intent
+            Intent sendIntent = new Intent();
+            sendIntent.setAction(Intent.ACTION_SEND);
+            sendIntent.putExtra(Intent.EXTRA_TEXT, shareString);
+            sendIntent.setType("text/plain");
+            startActivity(sendIntent);
+
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
     }
 }
