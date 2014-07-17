603dafff64c47ac1155c1c45042cdf67d24cfdd3
Anuchit Chalothorn <anoochit@gmail.com>
2014-07-17 07:41:04 +0700

    clear background

--- wear/src/main/java/net/redlinesoft/hellowear/WearActivity.java
+++ wear/src/main/java/net/redlinesoft/hellowear/WearActivity.java
@@ -3,45 +3,44 @@ package net.redlinesoft.hellowear;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
-import android.util.Log;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
 
 public class WearActivity extends Activity {
 
-    private TextView mTextDate;
-    private TextView mTextTime;
+    public TextView mTextDate;
+    public TextView mTextTime;
 
-    Timer timer;
-    MyTimerTask myTimerTask;
+    public Timer timer;
+    public MyTimerTask myTimerTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wear);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        WatchViewStub stub;
+        stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
                 mTextDate = (TextView) stub.findViewById(R.id.textDate);
                 mTextTime = (TextView) stub.findViewById(R.id.textTime);
-
+                if (timer != null) {
+                    timer.cancel();
+                }
                 timer = new Timer();
                 myTimerTask = new MyTimerTask();
-                
                 timer.schedule(myTimerTask, 1000, 1000);
             }
         });
     }
 
     class MyTimerTask extends TimerTask {
-
         @Override
         public void run() {
             Calendar calendar = Calendar.getInstance();
@@ -49,15 +48,13 @@ public class WearActivity extends Activity {
             SimpleDateFormat simpleTime = new SimpleDateFormat("HH:mm:ss");
             final String strDate = simpleDate.format(calendar.getTime());
             final String strTime = simpleTime.format(calendar.getTime());
-
-            runOnUiThread(new Runnable(){
-
+            runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
                     mTextDate.setText(strDate);
                     mTextTime.setText(strTime);
-                }});
+                }
+            });
         }
-
     }
 }
